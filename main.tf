93869bae8190df1d980616947ebd738c832c9bd5
KirillF <kirill.filimonov@gmail.com>
2022-06-18 15:49:30 +0300

    Update aws-iam-authenticator

--- main.tf
+++ main.tf
@@ -1,10 +1,9 @@
 provider "kubernetes" {
-  #load_config_file       = false
   cluster_ca_certificate = base64decode(var.kubernetes_cluster_cert_data)
   host                   = var.kubernetes_cluster_endpoint
 
   exec {
-    api_version = "client.authentication.k8s.io/v1alpha1"
+    api_version = "client.authentication.k8s.io/v1beta1"
     command     = "aws-iam-authenticator"
     args        = ["token", "-i", "${var.kubernetes_cluster_name}"]
   }
@@ -12,11 +11,10 @@ provider "kubernetes" {
 
 provider "helm" {
   kubernetes {
-    #load_config_file       = false
     cluster_ca_certificate = base64decode(var.kubernetes_cluster_cert_data)
     host                   = var.kubernetes_cluster_endpoint
     exec {
-      api_version = "client.authentication.k8s.io/v1alpha1"
+      api_version = "client.authentication.k8s.io/v1beta1"
       command     = "aws-iam-authenticator"
       args        = ["token", "-i", "${var.kubernetes_cluster_name}"]
     }
