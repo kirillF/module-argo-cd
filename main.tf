0bbe4992069edbddb84543d18b23add775cb8549
KirillF <kirill.filimonov@gmail.com>
2022-06-20 22:02:30 +0300

    revert version

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "kubernetes" {
   host                   = var.kubernetes_cluster_endpoint
   cluster_ca_certificate = base64decode(var.kubernetes_cluster_cert_data)
   exec {
-    api_version = "client.authentication.k8s.io/v1alpha1"
+    api_version = "client.authentication.k8s.io/v1beta1"
     args        = ["token", "-i", "${var.kubernetes_cluster_name}"]
     command     = "aws-iam-authenticator"
   }
@@ -13,7 +13,7 @@ provider "helm" {
     host                   = var.kubernetes_cluster_endpoint
     cluster_ca_certificate = base64decode(var.kubernetes_cluster_cert_data)
     exec {
-      api_version = "client.authentication.k8s.io/v1alpha1"
+      api_version = "client.authentication.k8s.io/v1beta1"
       args        = ["token", "-i", "${var.kubernetes_cluster_name}"]
       command     = "aws-iam-authenticator"
     }
