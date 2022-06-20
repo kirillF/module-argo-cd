8fa8458d493f394a98a5d3c4ecbd7bc7c1491c6a
KirillF <kirill.filimonov@gmail.com>
2022-06-20 20:51:15 +0300

    Downgrade api version

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "kubernetes" {
   host                   = var.kubernetes_cluster_endpoint
   cluster_ca_certificate = base64decode(var.kubernetes_cluster_cert_data)
   exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
+    api_version = "client.authentication.k8s.io/v11"
     args        = ["token", "-i", "${var.kubernetes_cluster_name}"]
     command     = "aws-iam-authenticator"
   }
@@ -13,16 +13,16 @@ provider "helm" {
     host                   = var.kubernetes_cluster_endpoint
     cluster_ca_certificate = base64decode(var.kubernetes_cluster_cert_data)
     exec {
-      api_version = "client.authentication.k8s.io/v1beta1"
+      api_version = "client.authentication.k8s.io/v11"
       args        = ["token", "-i", "${var.kubernetes_cluster_name}"]
       command     = "aws-iam-authenticator"
     }
   }
 }
 
-resource "kubernetes_namespace" "example" {
+resource "kubernetes_namespace" "argo-ns" {
   metadata {
-    name = "argo"
+    name = "argocd"
   }
 }
 
@@ -30,5 +30,5 @@ resource "helm_release" "argocd" {
   name       = "msur"
   chart      = "argo-cd"
   repository = "https://argoproj.github.io/argo-helm"
-  namespace  = "argo"
+  namespace  = "argocd"
 }
